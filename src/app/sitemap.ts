eef45fc0c0cbd0380daee1dce22813e7df4c3074
Marcos Iglesias <markkos89@gmail.com>
2024-08-29 05:35:20 -0300

    fix: deleted double slashes on tracks and lessons dynamic paths on sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -22,14 +22,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const tracksData = await api.tracks.getAll();
 
   const tracksDynamicPaths = tracksData.map((track) => ({
-    url: `https://academy.developerdao.com/${track.trackPath}`,
+    url: `https://academy.developerdao.com${track.trackPath}`,
     lastModified: lastModified,
   }));
 
   const lessonsData = await api.lessons.getAll();
 
   const lessonsDynamicPaths = lessonsData.map((lesson) => ({
-    url: `https://academy.developerdao.com/${lesson.lessonPath}`,
+    url: `https://academy.developerdao.com${lesson.lessonPath}`,
     lastModified: lastModified,
   }));
 
