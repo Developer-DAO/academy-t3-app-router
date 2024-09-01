3345d5770e2ccc030196a39031b538c9d16522bf
Marcos Iglesias <markkos89@gmail.com>
2024-09-01 04:04:25 -0300

    chore: debuginging

--- src/app/fundamentals/[lessonName]/page.tsx
+++ src/app/fundamentals/[lessonName]/page.tsx
@@ -3,7 +3,8 @@ import { GetLessonContentByTrackAndLessonName } from "@/lib/lessons";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import Components from "@/components/mdx/Components";
 import { type Lessons } from "@prisma/client";
-import { getBaseUrl } from "@/lib/url";
+// import { getBaseUrl } from "@/lib/url";
+import { db } from "@/server/db";
 
 type Props = {
   params: { lessonName: string };
@@ -13,14 +14,24 @@ export const dynamic = "force-dynamic";
 
 export async function generateStaticParams() {
   // const url = process.env.VERCEL_ENV ? getBaseUrl() : "http://localhost:3000";
-  const url = getBaseUrl();
+  // const url = getBaseUrl();
 
-  console.log("URL FINAL LESSONS 2: ", url);
+  // console.log("URL FINAL LESSONS 2: ", url);
 
-  const lessons = await fetch(`${url}/api/lessons`, { method: "GET" }).then(
-    (res) => res.json(),
-  );
-  console.log({ lessons });
+  // const lessons = await fetch(`${url}/api/lessons`, { method: "GET" }).then(
+  //   (res) => res.json(),
+  // );
+
+  const lessons = await db.lessons.findMany({
+    where: {
+      lessonPath: {
+        contains: "fundamentals",
+      },
+    },
+  });
+
+  console.log("generateStaticParams ", { lessons });
+
   return lessons.map((lesson: Lessons) => ({
     lessonName: lesson.lessonPath.replace("/fundamentals/", ""),
   }));
