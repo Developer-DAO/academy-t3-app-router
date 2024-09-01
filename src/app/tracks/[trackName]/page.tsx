4249a5f61d61a5adbeff0dc6ff68f87da9ff9412
Marcos Iglesias <markkos89@gmail.com>
2024-08-31 21:40:43 -0300

    chore: debugging

--- src/app/tracks/[trackName]/page.tsx
+++ src/app/tracks/[trackName]/page.tsx
@@ -15,7 +15,7 @@ export async function generateStaticParams() {
   console.log("URL FINAL TRACKS: ", url);
 
   const tracks = await fetch(`${url}/api/tracks`).then((res) => res.json()); // TODO: DEV_NOTE: We have to create the "URL" environment variable which changes from local/development/production environment. LOCAL is localhost, DEVELOPMENT is the vercel dynamic url and PRODUCTION is the academy domain
-
+  console.log("tracksss ", { tracks });
   return tracks.map((track: Tracks) => ({
     trackName: track.trackPath.replace("/tracks/", ""),
   }));
