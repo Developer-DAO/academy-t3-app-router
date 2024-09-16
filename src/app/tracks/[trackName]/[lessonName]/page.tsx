994d6bbbc36b7b17da66852f076dcb1190644ff4
Marcos Iglesias <markkos89@gmail.com>
2024-09-16 08:00:51 -0300

    feat: fixed renderization of content for tracks lessons

--- src/app/tracks/[trackName]/[lessonName]/page.tsx
+++ src/app/tracks/[trackName]/[lessonName]/page.tsx
@@ -2,24 +2,26 @@ import { GetLessonContentByTrackAndLessonName } from "@/lib/lessons";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import Components from "@/components/mdx/Components";
-import { type Lessons } from "@prisma/client";
+import { type Lessons, type Tracks } from "@prisma/client";
 import { db } from "@/server/db";
 
 type Props = {
   params: { trackName: string; lessonName: string };
 };
 
+type LessonWithTrack = Lessons & {
+  track: Tracks;
+};
+
 export async function generateStaticParams({ params }: Props) {
   const lessons = await db.lessons.findMany({
-    where: {
-      lessonPath: {
-        contains: params.trackName,
-      },
+    include: {
+      track: true,
     },
   });
 
-  return lessons.map((lesson: Lessons) => ({
-    trackName: params.trackName,
+  return lessons.map((lesson: LessonWithTrack) => ({
+    trackName: lesson.track.trackPath.replace(`/tracks/`, ""),
     lessonName: lesson.lessonPath.replace(`/tracks/${params.trackName}/`, ""),
   }));
 }
