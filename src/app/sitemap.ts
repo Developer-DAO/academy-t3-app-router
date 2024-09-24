d0d7ac517ea738ace655c44e42b2de38026ee762
Marcos Iglesias <markkos89@gmail.com>
2024-09-24 14:22:10 -0300

    feat: preferred partner wallet config and dynamic domain on sitemap and robots generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,17 +4,22 @@ import type { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const lastModified = new Date();
 
+  const appUrl =
+    process.env.VERCEL_ENV === "production"
+      ? `https://academy-t3-app-router.vercel.app` // TODO: after domain update change it to: https://academy.developerdao.com
+      : `https://${process.env.VERCEL_URL}`;
+
   const staticPaths = [
     {
-      url: "https://academy-t3-app-router.vercel.app/", // `https://academy.developerdao.com/`,
+      url: appUrl, // `https://academy.developerdao.com/`,
       lastModified,
     },
     {
-      url: "https://academy-t3-app-router.vercel.app/fundamentals", // `https://academy.developerdao.com/fundamentals`,
+      url: `${appUrl}/fundamentals`, // `https://academy.developerdao.com/fundamentals`,
       lastModified,
     },
     {
-      url: "https://academy-t3-app-router.vercel.app/tracks", // `https://academy.developerdao.com/tracks`,
+      url: `${appUrl}/tracks`, // `https://academy.developerdao.com/tracks`,
       lastModified,
     },
   ];
@@ -22,14 +27,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const tracksData = await api.tracks.getAll();
 
   const tracksDynamicPaths = tracksData.map((track) => ({
-    url: `https://academy-t3-app-router.vercel.app${track.trackPath}`, // `https://academy.developerdao.com${track.trackPath}`,
+    url: `${appUrl}${track.trackPath}`, // `https://academy.developerdao.com${track.trackPath}`,
     lastModified: lastModified,
   }));
 
   const lessonsData = await api.lessons.getAll();
 
   const lessonsDynamicPaths = lessonsData.map((lesson) => ({
-    url: `https://academy-t3-app-router.vercel.app${lesson.lessonPath}`, // `https://academy.developerdao.com${lesson.lessonPath}`,
+    url: `${appUrl}${lesson.lessonPath}`, // `https://academy.developerdao.com${lesson.lessonPath}`,
     lastModified: lastModified,
   }));
 
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,39 @@
 import { env } from "@/env";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet /*, optimism, optimismSepolia */ } from "wagmi/chains";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { createConfig, http } from "wagmi";
+import { zerionWallet } from "@rainbow-me/rainbowkit/wallets";
+import { createClient } from "viem";
 
-export const config = getDefaultConfig({
-  appName: "RainbowKit demo",
-  projectId: env.NEXT_PUBLIC_WALLET_CONNECT_ID,
-  chains: [
-    mainnet,
-    // optimism,
-    // ...(env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [optimismSepolia] : []),
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "D_D Preferred Partner",
+      wallets: [zerionWallet],
+    },
   ],
+  {
+    appName: "D_D Academy",
+    projectId: env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+  },
+);
+
+export const config = createConfig({
+  chains: [mainnet],
   ssr: true,
+  connectors,
+  client({ chain }) {
+    return createClient({ chain, transport: http() });
+  },
 });
+
+// export const config = getDefaulConfig({
+//   appName: "D_D Academy",
+//   projectId: env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+//   chains: [
+//     mainnet,
+//     // optimism,
+//     // ...(env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [optimismSepolia] : []),
+//   ],
+//   ssr: true,
+// });
