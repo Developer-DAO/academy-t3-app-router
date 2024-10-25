7d9a91df0896cf03b437a0eebd0461c461580575
Rohan Ahire <75326769+rohan-ahire06@users.noreply.github.com>
2024-10-25 12:34:57 +0530

    feat: added the section for 'not started yet'

--- src/app/myProfile/page.tsx
+++ src/app/myProfile/page.tsx
@@ -1,53 +1,72 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Link } from "lucide-react";
 import ProfileComponentContainer from "@/components/ProfileComponentContainer";
 import { Icons } from "@/components/Icons";
+import { Button } from "@/components/Button";
+import { useRouter } from "next/navigation";
 
 const MyProfile = () => {
+  const router = useRouter();
   return (
     <div className=" flex flex-col  md:mx-[90px] md:mt-[120px]">
       <div className=" text-bttf text-[32px] text-white">My Profile</div>
-      <ProfileComponentContainer style="md:mt-[40px] md:mb-[30px] flex flex-col items-center justify-center">
-        <Image
-          src={"/avatar.png"}
-          alt="account avatar"
-          width={50}
-          height={50}
-          className="border-3 w-auto rounded-full border-black object-cover p-0 opacity-80 shadow-sm hover:border-amber-400 md:h-[210px]"
-        />
-        <p className="  mb-[5px] mt-[25px] font-bold md:text-[32px]">
-          John Smith
-        </p>
-        <p className=" flex items-center gap-x-2 text-sm">
-          Share
-          <Link size={16} />
-        </p>
-      </ProfileComponentContainer>
-      <ProfileComponentContainer style="font-bold flex flex-col gap-y-[44px]">
-        <div className=" text-[32px]">Stats</div>
+      <div className=" flex  md:gap-x-[300px]">
+        <div>
+          <ProfileComponentContainer style="md:mt-[40px] md:mb-[30px] flex flex-col items-center justify-center">
+            <Image
+              src={"/avatar.png"}
+              alt="account avatar"
+              width={50}
+              height={50}
+              className="border-3 w-auto rounded-full border-black object-cover p-0 opacity-80 shadow-sm hover:border-amber-400 md:h-[210px]"
+            />
+            <p className="  mb-[5px] mt-[25px] font-bold md:text-[32px]">
+              John Smith
+            </p>
+            <p className=" flex items-center gap-x-2 text-sm">
+              Share
+              <Link size={16} />
+            </p>
+          </ProfileComponentContainer>
+          <ProfileComponentContainer style="font-bold flex flex-col gap-y-[44px] md:mb-[225px]">
+            <div className=" text-[32px]">Stats</div>
 
-        <div className=" flex flex-col gap-y-[25px] font-medium md:text-[20px]">
-          <div className=" flex items-center justify-between">
-            <div className=" flex items-center gap-x-[25px]">
-              <Icons.progress className=" h-[30px] w-[30px]" /> In Progress
+            <div className=" flex flex-col gap-y-[25px] font-medium md:text-[20px]">
+              <div className=" flex items-center justify-between">
+                <div className=" flex items-center gap-x-[25px]">
+                  <Icons.progress className=" h-[30px] w-[30px]" /> In Progress
+                </div>
+                <p>0</p>
+              </div>
+              <div className=" flex items-center justify-between">
+                <div className=" flex items-center gap-x-[25px]">
+                  <Icons.completed className=" h-[30px] w-[30px]" /> Completed
+                </div>
+                <p>0</p>
+              </div>
+              <div className=" flex items-center justify-between">
+                <div className=" flex items-center gap-x-[25px]">
+                  <Icons.nftminted className=" h-[30px] w-[30px]" /> NFTs Minted
+                </div>
+                <p>0</p>
+              </div>
             </div>
-            <p>0</p>
-          </div>
-          <div className=" flex items-center justify-between">
-            <div className=" flex items-center gap-x-[25px]">
-              <Icons.completed className=" h-[30px] w-[30px]" /> Completed
-            </div>
-            <p>0</p>
-          </div>
-          <div className=" flex items-center justify-between">
-            <div className=" flex items-center gap-x-[25px]">
-              <Icons.nftminted className=" h-[30px] w-[30px]" /> NFTs Minted
-            </div>
-            <p>0</p>
-          </div>
+          </ProfileComponentContainer>
         </div>
-      </ProfileComponentContainer>
+        <div className=" flex flex-col items-center gap-y-[50px] md:mt-[100px]">
+          <p className=" text-center font-poppins text-[48px] font-bold text-white">
+            Looks like you haven’t <br /> started a track yet
+          </p>
+          <Button
+            onClick={() => router.push("/tracks")}
+            className={`connect-button !px-7 !py-3 font-future hover:bg-[var(--button-secondary-dark)]`}
+          >
+            Get Started
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
